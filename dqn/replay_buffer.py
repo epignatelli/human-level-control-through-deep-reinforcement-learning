5f3e131950201c90fd7085a08f7530a44a25510d
epignatelli <edu.pignatelli@gmail.com>
2020-11-27 11:32:04 +0000

    fix typo

--- dqn/replay_buffer.py
+++ dqn/replay_buffer.py
@@ -31,7 +31,7 @@ class ReplayBuffer:
     def add(self, timestep, action, new_timestep):
         self._data.append(
             Transition(
-                timestep.observation, action, timestep.reward, new_timestep.observations
+                timestep.observation, action, timestep.reward, new_timestep.observation
             )
         )
         return
